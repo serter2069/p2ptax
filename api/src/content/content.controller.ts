dac32c94a3fcc7307f6bed2ff0b365b9118e5807
serter2069 <serter2069@gmail.com>
2026-04-13 21:20:39 +0200

    Fix issue #667: Implement: Terms of Service screen + API endpoint

---
+++ api/src/content/content.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { ContentService } from './content.service';
+
+@Controller('content')
+export class ContentController {
+  constructor(private readonly contentService: ContentService) {}
+
+  @Get('terms')
+  getTerms() {
+    return this.contentService.getTerms();
+  }
+}
